534e818f9d040fe91ee48f6b38b343320bb0fe1e
ilyas20032011 <ilyas20032011@hotmail.fr>
2025-03-26 05:08:45 +0100

    25/03

--- front-end-we-see/src/app/login/page.js
+++ front-end-we-see/src/app/login/page.js
@@ -1,10 +1,48 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Login() {
   return (
-    <h1 className="text-3xl font-bold text-white underline">
-      Hello world!
-      login page
-    </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">Se connecter</h1>
+
+        {/* Login*/}
+        <form className="space-y-4">
+          
+          <div>
+            <label htmlFor="email" className="block text-sm text-gray-700">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          
+          <div>
+            <label htmlFor="password" className="block text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          {/* Submit*/}
+          <div>
+            <button
+              type="submit"
+              className="w-full py-3 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
--- front-end-we-see/src/app/register/page.js
+++ front-end-we-see/src/app/register/page.js
@@ -1,10 +1,71 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Register() {
   return (
-    <h1 className="text-3xl font-bold text-white underline">
-      Hello world!
-      register page
-    </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">Créer un Compte</h1>
+
+        {/* creer compte Form */}
+        <form className="space-y-4">
+         
+          <div>
+            <label htmlFor="name" className="block text-sm text-gray-700">Nom complet</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Enter your full name"
+            />
+          </div>
+
+      
+          <div>
+            <label htmlFor="email" className="block text-sm text-gray-700">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="confirmPassword" className="block text-sm text-gray-700">Confirmer Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              required
+              className="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none text-black"
+              placeholder="Confirm your password"
+            />
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="w-full py-3 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none"
+            >
+              Créer un Compte
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
