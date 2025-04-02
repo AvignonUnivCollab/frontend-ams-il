a30d87b6fa22e480d5ef5c6eccbde8eddf22f3b0
ilyas benlyazid <71908714+azertyco@users.noreply.github.com>
2025-04-02 09:24:00 +0200

    Update page.js

--- front-end-we-see/src/app/login/page.js
+++ front-end-we-see/src/app/login/page.js
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import axios from "axios";
-s
 export default function Login() {
   const [username, setusername] = useState(""); 
   const [password, setPassword] = useState("");
